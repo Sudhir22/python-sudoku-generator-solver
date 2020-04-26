a4aa569f4598d8fc0b3e365499778408bc7c69c7
Vora <u6415626@cbenet.anu.edu.au>
2020-04-26 16:48:58 +1000

    2-p

--- sudoku/sudoku_solver/urls.py
+++ sudoku/sudoku_solver/urls.py
@@ -9,6 +9,8 @@ urlpatterns=[
     path('homepage/',views.homepage,name='homepage'),
     path('homepage_2/',views.homepage_2,name='homepage_2'),
     path('task/',views.task,name='task'),
+    path('task_2/',views.task,name='task_2'),
     path('second_task/',views.second_task,name='second_task'),
+    path('second_task_2/',views.second_task,name='second_task_2'),
     url(r'^end-game/?',views.end_game, name='end-game'),
 ] 
